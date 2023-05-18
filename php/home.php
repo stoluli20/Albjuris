5158074512a13a4048123e5fb140f14613f73c7f
klaudia rapaj <klaudiaxrapaj@gmail.com>
2023-05-18 23:55:23 +0200

    committing changes

--- php/home.php
+++ php/home.php
@@ -1,31 +1,15 @@
+
 <html>
+
   <head>
-    <?php require __DIR__ . '/../html/header.html'?>
-    <link rel="stylesheet" href="/Web_Project/css/styleimg.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <title> Alb Juris | Home</title>
+
   </head>
   <body>
-    <div class="jumbotron bg-secondary">
-      <h1 style="text-align:center;"class="display-4 text-white">Welcome to Our Home Page</h1>
-      <p style="text-align:center;"class="lead text-white">Go through our Properties for Sale and Rent</p>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <h2 style="text-align:center;"class="text-primary">Explore our properties</h2>
-        </div>   
-    </div>
-  </div>
 
-  <div class="container">
-    <img src="image/realest1.jpg" alt="">
-    <img src="image/realest2.jpg" alt="">
-    <img src="image/realest3.jpg" alt="">
-    <img src="image/realest4.jpg" alt="">
-  </div>
-<?php require __DIR__ . '/../html/footer.html'?>
+
+ 
+
 </body>
+
 </html>
